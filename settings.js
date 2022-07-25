42e9275f53b03f8c2848713036e8dbe766e342ff
hirujahirusara <109913665+hirujahirusara@users.noreply.github.com>
2022-07-25 21:35:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['70 124 4595]
+global.premium = [''70 124 4595']
+global.ownernomer = ''70 124 4595'
+global.ownername = '🙂HIRU'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.ig = 'https://chat.whatsapp.com/FUCnI6XLMDbHkVjrv6pr5F'
+global.region = 'Srilanka, Western,Colombo 
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
 global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.author = '🐦HIHIRUJA HIRUSARA '
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
